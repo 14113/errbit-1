da710c662cc50668eaf550d6fe629143fb2d6811
Adam MArtinik <a.martinik@gmail.com>
2015-06-04 16:36:37 +0200

    Config

--- config/initializers/_load_config.rb
+++ config/initializers/_load_config.rb
@@ -37,6 +37,7 @@ unless defined?(Errbit::Config)
       :authentication => :plain,
       :user_name      => ENV['SMTP_USERNAME']   || ENV['SENDGRID_USERNAME'],
       :password       => ENV['SMTP_PASSWORD']   || ENV['SENDGRID_PASSWORD'],
+      :enable_starttls_auto => true,
       :domain         => ENV['SMTP_DOMAIN'] || ENV['SENDGRID_DOMAIN'] ||
                            (ENV['ERRBIT_EMAIL_FROM'] ? ENV['ERRBIT_EMAIL_FROM'].split('@').last : nil)
     }
